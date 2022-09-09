0606c8e22772ac0d290f451b373b2e89ff7dc49c
csaszy <csaszy2006@gmail.com>
2022-09-09 19:16:05 +0200

    bg

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-def main():
-    pass
+import time
+mtx = []
+cols = 10
+rows = 20
 
-if __name__ == "name":
+bg=" "
+cube="#"
+#----------------------------------------------------------------
+def main():
+    gen()
+    while True:
+        Update()
+        time.sleep(1)
+#----------------------------------------------------------------
+def gen():
+    for i in range(rows):
+        row = []
+        for j in range(cols):
+            row.append(bg)
+        mtx.append(row)
+#----------------------------------------------------------------
+def Update():
+    for row in mtx:
+        for col in row:
+            print(bg,end="")
+        print("")
+    print("")
+#----------------------------------------------------------------
+if __name__ == "__main__":
     main()
